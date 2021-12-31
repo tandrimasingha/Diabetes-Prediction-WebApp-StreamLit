e2cb0ab4cbc233cc2f92f3ef0e980326f8c2585b
Tandrima Singha <78292851+tandrimasingha@users.noreply.github.com>
2021-12-31 17:15:27 +0530

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -32,7 +32,7 @@ def main():
     
     # giving title
     st.title('Diabetes Prediction Web App')
-    st.image("diabetes.png",width=200)
+    st.image("diabetes.png",width=200,align='center')
     
     #getting the input data from the user
     
