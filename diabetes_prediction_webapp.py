0a35158bf95a28603c84ea8507d6a3bc46550af9
Tandrima Singha <78292851+tandrimasingha@users.noreply.github.com>
2021-12-31 15:46:57 +0530

    Add files via upload

---
+++ diabetes_prediction_webapp.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 31 12:21:49 2021
+
+@author: TANDRIMA SINGHA
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+loaded_model = pickle.load(open('F:/Deploying Models/trained_model.sav', 'rb'))
+
+# creating a function for prediction
+def diabetes_prediction(input_data):
+    
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+      return 'The person is not diabetic'
+    else:
+      return 'The person is diabetic'
+  
+def main():
+    
+    # giving title
+    st.title('Diabetes Prediction Web App')
+    
+    #getting the input data from the user
+    
+    Pregnancies = st.text_input('Number of Pregnencies')
+    Glucose = st.text_input('Glucose Level')
+    BloodPressure = st.text_input('Blood Pressure Value')
+    SkinThickness = st.text_input('Skin Thickness Value')
+    Insulin = st.text_input('Insulin Level')
+    BMI = st.text_input('BMI value')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    Age = st.text_input('Age of the person')
+    
+    #code for prediction 
+    diagnosis = ''
+    
+    #creating a button for prediction
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
+    
+    st.success(diagnosis)
+    
+if __name__=='__main__':
+    main()
